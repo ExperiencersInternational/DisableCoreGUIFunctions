e7b2b93a48190e4b57100cef501913d86f7d5c10
Ty <74572291+TyNoOutlet@users.noreply.github.com>
2021-07-22 10:07:00 -0400

    Updated the plugin

--- DisableRobloxCoreGUIFunctions.lua
+++ DisableRobloxCoreGUIFunctions.lua
@@ -1,118 +1,250 @@
-local toolbar = plugin:CreateToolbar("Disable CoreGUI Functions v1.05 by GamersInternational")
+local toolbar = plugin:CreateToolbar("DisableCoreGUIFunctions v1.05")
+local SP = game:GetService("StarterPlayer")
+local SPS = SP.StarterPlayerScripts
+local CHS = game:GetService("ChangeHistoryService")
+local openUIPanel = toolbar:CreateButton("Disable Function Menu", "This button opens a control panel.", "rbxassetid://4458901886")
 
-local disablechatbutton = toolbar:CreateButton("Disable Chat", "This button inserts a LocalScript that disables chat.", "rbxassetid://4458901886")
-local disablehealthbarbutton = toolbar:CreateButton("Disable Health Bar", "This button inserts a LocalScript that disables the health bar popping up next to the menu button when you take damage in a game.", "rbxassetid://4458901886")
-local disablebackpackbutton = toolbar:CreateButton("Disable Backpack", "This button inserts a LocalScript that disables the backpack function in game. When disabling, any tools that go into the backpack will not work unless you have an alternative backpack.", "rbxassetid://4458901886")
-local disableplayerlistbutton = toolbar:CreateButton("Disable Leaderboard/PlayerList", "This button inserts a LocalScript that disables the Roblox leaderboard/playerlist.", "rbxassetid://4458901886")
-local disableemotesbutton = toolbar:CreateButton("Disable Emotes", "This button inserts a LocalScript that disables the emotes GUI. Players will still be able to use emotes if they type them into the Roblox chat.", "rbxassetid://4458901886")
-local disableresetbutton = toolbar:CreateButton("Disable Reset Character", "This button inserts a LocalScript into StarterGUI that disables the reset character button in the Roblox menu.", "rbxassetid://4458901886")
-local disableshiftlockbutton = toolbar:CreateButton("Disable Shift Lock Switch", "This button inserts a Script into Workspace that disables the shift lock switch toggle in the Roblox menu.", "rbxassetid://4458901886")
-local disableallbutton = toolbar:CreateButton("Disable All CoreGUI Functions","This button inserts a LocalScript that disables every CoreGUI function listed in the other buttons.","rbxassetid://4458901886")
--- local readmedocument = toolbar:CreateButton("ReadMe Information","Inserts a readme document into the Workplace menu.")
---readmedocument.Click:Connect (function()
--- local newreadmescript = Instance.new("Script")
--- newreadmescript.Name = "DisableCoreGUIFunctionsReadMe"
---end)
-disablechatbutton.Click:Connect (function()
-	if game.StarterPlayer.StarterPlayerScripts:FindFirstChild("DisableChat") then
-		return
-	else
-		local newdisablechatscript = Instance.new("LocalScript")
-		newdisablechatscript.Source = "local StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)"
-		newdisablechatscript.Parent = game.StarterPlayer.StarterPlayerScripts
-		newdisablechatscript.Name = "DisableChat"
+local widgetInfo = DockWidgetPluginGuiInfo.new(
+	Enum.InitialDockState.Float,
+	true,
+	true,
+	400,
+	400,
+	50,
+	50
+)
+local ui = plugin:CreateDockWidgetPluginGui("Widget", widgetInfo)
+ui.Title = "DisableCoreGUIFunctions"
+local fr = Instance.new("Frame")
+fr.BackgroundColor3 = Color3.fromRGB(55,55,55)
+fr.BorderSizePixel = 0
+fr.Size = UDim2.new(1, 0, 1, 0)
+fr.Parent = ui
+local name = Instance.new("TextLabel")
+name.BorderSizePixel = 0
+name.BackgroundColor3 = Color3.fromRGB(55,55,55)
+name.Name = "Name" -- funniest line of code
+name.Position = UDim2.new(0, 0, 0, 0)
+name.Size = UDim2.new(1, 0, 0.1, 0)
+name.Font = Enum.Font.Nunito
+name.Text = "DisableCoreGUIFunctions"
+name.TextColor3 = Color3.new(1, 1, 1)
+name.TextScaled = true
+name.Parent = ui
+local chat = Instance.new("TextButton")
+chat.BorderSizePixel = 0
+chat.BackgroundColor3 = Color3.fromRGB(55,55,55)
+chat.Name = "Chat"
+chat.Position = UDim2.new(0, 0, 0.15, 0)
+chat.Size = UDim2.new(0.45, 0, 0.125, 0)
+chat.Font = Enum.Font.Nunito
+chat.Text = "Disable Chat"
+chat.TextColor3 = Color3.new(1, 1, 1)
+chat.TextScaled = true
+chat.Parent = ui
+local backpack = Instance.new("TextButton")
+backpack.BorderSizePixel = 0
+backpack.BackgroundColor3 = Color3.fromRGB(55,55,55)
+backpack.Name = "Backpack"
+backpack.Position = UDim2.new(0, 0, 0.275, 0)
+backpack.Size = UDim2.new(0.45, 0, 0.125, 0)
+backpack.Font = Enum.Font.Nunito
+backpack.Text = "Disable Backpack"
+backpack.TextColor3 = Color3.new(1, 1, 1)
+backpack.TextScaled = true
+backpack.Parent = ui
+local emotes = Instance.new("TextButton")
+emotes.BorderSizePixel = 0
+emotes.BackgroundColor3 = Color3.fromRGB(55,55,55)
+emotes.Name = "EmoteList"
+emotes.Position = UDim2.new(0, 0, 0.4, 0)
+emotes.Size = UDim2.new(0.45, 0, 0.125, 0)
+emotes.Font = Enum.Font.Nunito
+emotes.Text = "Disable Emotes"
+emotes.TextColor3 = Color3.new(1, 1, 1)
+emotes.TextScaled = true
+emotes.Parent = ui
+local health = Instance.new("TextButton")
+health.BorderSizePixel = 0
+health.BackgroundColor3 = Color3.fromRGB(55,55,55)
+health.Name = "Health"
+health.Position = UDim2.new(0.55, 0, 0.15, 0)
+health.Size = UDim2.new(0.45, 0, 0.125, 0)
+health.Font = Enum.Font.Nunito
+health.Text = "Disable Health Bar"
+health.TextColor3 = Color3.new(1, 1, 1)
+health.TextScaled = true
+health.Parent = ui
+local plrList = Instance.new("TextButton")
+plrList.BorderSizePixel = 0
+plrList.BackgroundColor3 = Color3.fromRGB(55,55,55)
+plrList.Name = "PlayerList"
+plrList.Position = UDim2.new(0.55, 0, 0.275, 0)
+plrList.Size = UDim2.new(0.45, 0, 0.125, 0)
+plrList.Font = Enum.Font.Nunito
+plrList.Text = "Disable Leaderboard"
+plrList.TextColor3 = Color3.new(1, 1, 1)
+plrList.TextScaled = true
+plrList.Parent = ui
+local reset = Instance.new("TextButton")
+reset.BorderSizePixel = 0
+reset.BackgroundColor3 = Color3.fromRGB(55,55,55)
+reset.Name = "Reset"
+reset.Position = UDim2.new(0.55, 0, 0.4, 0)
+reset.Size = UDim2.new(0.45, 0, 0.125, 0)
+reset.Font = Enum.Font.Nunito
+reset.Text = "Disable Reset"
+reset.TextColor3 = Color3.new(1, 1, 1)
+reset.TextScaled = true
+reset.Parent = ui
+local shift = Instance.new("TextButton")
+shift.BorderSizePixel = 0
+shift.BackgroundColor3 = Color3.fromRGB(55,55,55)
+shift.Name = "ShiftLock"
+shift.Position = UDim2.new(0, 0, 0.54, 0)
+shift.Size = UDim2.new(1, 0, 0.125, 0)
+shift.Font = Enum.Font.Nunito
+shift.Text = "Disable Shift Lock"
+shift.TextColor3 = Color3.new(1, 1, 1)
+shift.TextScaled = true
+shift.Parent = ui
+local all = Instance.new("TextButton")
+all.BorderSizePixel = 0
+all.BackgroundColor3 = Color3.fromRGB(14, 147, 0)
+all.Name = "All"
+all.Position = UDim2.new(0, 0, 0.712, 0)
+all.Size = UDim2.new(1, 0, 0.287, 0)
+all.Font = Enum.Font.Nunito
+all.Text = "Disable All CoreGUI Functions"
+all.TextColor3 = Color3.new(1, 1, 1)
+all.TextScaled = true
+all.Parent = ui
+ui.Enabled = false
+
+local function click()
+    ui.Enabled = not ui.Enabled
+local uiElements = {
+	['Chat'] = chat;
+	['Health'] = health;
+	['Backpack'] = backpack;
+	['PlayerList'] = plrList;
+	['EmotesMenu'] = emotes;
+	['All'] = all
+}
+
+local folder, xfolder, yfolder
+
+if not SPS:FindFirstChild("DisableCoreGUIFunctions") or not game.StarterGui:FindFirstChild("DisableCoreGUIFunctions") or not workspace:FindFirstChild("DisableCoreGUIFunctions") then
+	folder = SPS:FindFirstChild("DisableCoreGUIFunctions") or Instance.new("Folder")
+	folder.Name = "DisableCoreGUIFunctions"
+	folder.Parent = SPS
+	xfolder = game.StarterGui:FindFirstChild("DisableCoreGUIFunctions") or Instance.new("Folder")
+	xfolder.Name = "DisableCoreGUIFunctions"
+	xfolder.Parent = game.StarterGui
+	yfolder = workspace:FindFirstChild("DisableCoreGUIFunctions") or Instance.new("Folder")
+	yfolder.Name = "DisableCoreGUIFunctions"
+	yfolder.Parent = workspace
+else
+    folder = SPS.DisableCoreGUIFunctions
+    xfolder = game.StarterGui:FindFirstChild("DisableCoreGUIFunctions")
+    yfolder = workspace:FindFirstChild("DisableCoreGUIFunctions")
+end
+
+for k, v in pairs(uiElements) do
+	v.Activated:Connect(function()
+		if folder.Parent == nil then
+			folder = Instance.new("Folder")
+			folder.Name = "DisableCoreGUIFunctions"
+			folder.Parent = SPS
+		end
+		if xfolder.Parent == nil then
+			xfolder = Instance.new("Folder")
+			xfolder.Name = "DisableCoreGUIFunctions"
+			xfolder.Parent = game.StarterGui
+		end
+		if yfolder.Parent == nil then
+			yfolder = Instance.new("Folder")
+			yfolder.Name = "DisableCoreGUIFunctions"
+			yfolder.Parent = workspace
+		end
+		if folder:FindFirstChild("Disable" .. k) then
+			return
+		else
+			CHS:SetWaypoint("Adding disabling scripts")
+			local newdisablescript = Instance.new("LocalScript")
+			newdisablescript.Source = "-- Try my new plugin, Roblox Fast Donation Button Creation at https://www.roblox.com/catalog/6016161703/Roblox-Fast-Donation-Button-Creation \n\nlocal StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType." .. k .. ", false)"
+			newdisablescript.Parent = folder
+			newdisablescript.Name = "Disable" .. k
+			if k == "All" then
+				local newdisableresetscript = Instance.new("LocalScript")
+				newdisableresetscript.Source = "local StarterGui = game:GetService('StarterGui') StarterGui:SetCore('ResetButtonCallback',false)"
+				newdisableresetscript.Parent = xfolder
+				newdisableresetscript.Name = "DisableResetCharacter"
+				local newdisableshiftlockscript = Instance.new("Script")
+				newdisableshiftlockscript.Source = "game.StarterPlayer.EnableMouseLockOption = false"
+				newdisableshiftlockscript.Parent = yfolder
+				newdisableshiftlockscript.Name = "DisableShiftLockSwitch"
+			end
+			CHS:SetWaypoint("Added disabling scripts")
+		end
+	end)
+end
+
+reset.Activated:Connect(function()
+	if folder.Parent == nil then
+		folder = Instance.new("Folder")
+		folder.Name = "DisableCoreGUIFunctions"
+		folder.Parent = SPS
 	end
-end)
-
-disablehealthbarbutton.Click:Connect (function()
-	if game.StarterPlayer.StarterPlayerScripts:FindFirstChild("DisableHealthBar") then
-		return
-	else
-		local newdisablehealthscript = Instance.new("LocalScript")
-		newdisablehealthscript.Source = "local StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)"
-		newdisablehealthscript.Parent = game.StarterPlayer.StarterPlayerScripts
-		newdisablehealthscript.Name = "DisableHealthBar"
+	if xfolder.Parent == nil then
+		xfolder = Instance.new("Folder")
+		xfolder.Name = "DisableCoreGUIFunctions"
+		xfolder.Parent = game.StarterGui
 	end
-end)
-
-disablebackpackbutton.Click:Connect (function()
-	if game.StarterPlayer.StarterPlayerScripts:FindFirstChild("DisableBackpack") then
-		return
-	else
-		local newdisablebackpackscript = Instance.new("LocalScript")
-		newdisablebackpackscript.Source = "local StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)"
-		newdisablebackpackscript.Parent = game.StarterPlayer.StarterPlayerScripts
-		newdisablebackpackscript.Name = "DisableBackpack"
+	if yfolder.Parent == nil then
+		yfolder = Instance.new("Folder")
+		yfolder.Name = "DisableCoreGUIFunctions"
+		yfolder.Parent = workspace
 	end
-end)
-
-disableplayerlistbutton.Click:Connect (function()
-	if game.StarterPlayer.StarterPlayerScripts:FindFirstChild("DisablePlayerlist") then
+	if xfolder:FindFirstChild("DisableResetCharacter") then
 		return
 	else
-		local newdisableplayerlistscript = Instance.new("LocalScript")
-		newdisableplayerlistscript.Source = "local StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)"
-		newdisableplayerlistscript.Parent = game.StarterPlayer.StarterPlayerScripts
-		newdisableplayerlistscript.Name = "DisablePlayerlist"
-	end
-end)
-
-disableemotesbutton.Click:Connect (function()
-	if game.StarterPlayer.StarterPlayerScripts:FindFirstChild("DisableEmotes") then
-		return
-	else
-	local newdisableemotesscript = Instance.new("LocalScript")
-	newdisableemotesscript.Source = "local StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)"
-	newdisableemotesscript.Parent = game.StarterPlayer.StarterPlayerScripts
-	newdisableemotesscript.Name = "DisableEmotes"
-	end
-end)
-
-disableallbutton.Click:Connect (function()
-local newdisableallscript = Instance.new("LocalScript")
-	newdisableallscript.Source = "-- Try my new plugin, Roblox Fast Donation Button Creation at https://www.roblox.com/catalog/6016161703/Roblox-Fast-Donation-Button-Creation \n\nlocal StarterGui = game:GetService('StarterGui') \ngame.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)"
-newdisableallscript.Parent = game.StarterPlayer.StarterPlayerScripts
-	newdisableallscript.Name = "DisableAllCoreGUIFunctions"
-	local newdisableresetscript = Instance.new("LocalScript")
-	local folder = Instance.new('Folder')
-	folder.Parent = game.StarterGui
-	folder.Name = 'DisableCoreGUIFunctions'
-	newdisableresetscript.Source = "local StarterGui = game:GetService('StarterGui') StarterGui:SetCore('ResetButtonCallback',false)"
-	newdisableresetscript.Parent = folder
-	newdisableresetscript.Name = "DisableResetCharacter"
-	local newdisableshiftlockscript = Instance.new("Script")
-	local wfolder = Instance.new('Folder')
-	wfolder.Parent = game.Workspace
-	wfolder.Name = 'DisableCoreGUIFunctions'
-	newdisableshiftlockscript.Source = "game.StarterPlayer.EnableMouseLockOption = false"
-	newdisableshiftlockscript.Parent = wfolder
-	newdisableshiftlockscript.Name = "DisableShiftLockSwitch"
-end)
-
-disableresetbutton.Click:Connect (function()
-	if game.StarterGui.DisableCoreGUIFunctions:FindFirstChild("DisableResetCharacter") then
-		return
-	else
+		CHS:SetWaypoint("Adding disabling scripts")
 		local newdisableresetscript = Instance.new("LocalScript")
-		local folder = Instance.new('Folder')
-		folder.Parent = game.StarterGui
-		folder.Name = 'DisableCoreGUIFunctions'
-		newdisableresetscript.Source = "local StarterGui = game:GetService('StarterGui') \nStarterGui:SetCore('ResetButtonCallback',false)"
-		newdisableresetscript.Parent = folder
+		newdisableresetscript.Source = "local StarterGui = game:GetService('StarterGui') StarterGui:SetCore('ResetButtonCallback',false)"
+		newdisableresetscript.Parent = xfolder
 		newdisableresetscript.Name = "DisableResetCharacter"
+		CHS:SetWaypoint("Added disabling scripts")
 	end
 end)
 
-disableshiftlockbutton.Click:Connect (function()
-	if workspace.DisableCoreGUIFunctions:FindFirstChild("DisableShiftLockSwitch") then
+shift.Activated:Connect(function()
+	if folder.Parent == nil then
+		folder = Instance.new("Folder")
+		folder.Name = "DisableCoreGUIFunctions"
+		folder.Parent = SPS
+	end
+	if xfolder.Parent == nil then
+		xfolder = Instance.new("Folder")
+		xfolder.Name = "DisableCoreGUIFunctions"
+		xfolder.Parent = game.StarterGui
+	end
+	if yfolder.Parent == nil then
+		yfolder = Instance.new("Folder")
+		yfolder.Name = "DisableCoreGUIFunctions"
+		yfolder.Parent = workspace
+	end
+	if folder:FindFirstChild("DisableShiftLockSwitch") then
 		return
 	else
+		CHS:SetWaypoint("Adding disabling scripts")
 		local newdisableshiftlockscript = Instance.new("Script")
-		local folder = Instance.new('Folder')
-		folder.Parent = game.Workspace
-		folder.Name = 'DisableCoreGUIFunctions'
 		newdisableshiftlockscript.Source = "game.StarterPlayer.EnableMouseLockOption = false"
-		newdisableshiftlockscript.Parent = folder
+		newdisableshiftlockscript.Parent = yfolder
 		newdisableshiftlockscript.Name = "DisableShiftLockSwitch"
+		CHS:SetWaypoint("Added disabling scripts")
 	end
 end)
+end
+
+openUIPanel.Click:Connect(click)
